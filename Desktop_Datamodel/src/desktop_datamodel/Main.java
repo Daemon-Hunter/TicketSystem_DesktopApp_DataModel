41336e6d5ec28c7204b948b0e21ed3d344a57ccc
cgillions <cjgillions@aol.com>
2016-03-16 02:03:09 +0000

    Couple of lambda expressions in the Main if you wanted to check out their layout. Haven't implemented in datamodel as I'm not 100% on whether we're using the same model for the Android app.

--- Desktop_Datamodel/src/desktop_datamodel/Main.java
+++ Desktop_Datamodel/src/desktop_datamodel/Main.java
@@ -5,17 +5,39 @@
  */
 package desktop_datamodel;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author 10512691
  */
 public class Main {
+    
+    public static void sumArraySquared(ArrayList<Integer> input) {
+        System.out.println(input.stream()
+                                .map(i -> i * i)
+                                .reduce(0, (c, i) -> c + i));
+    }
+    
+    public static void printValues(ArrayList<Integer> input) {
+        input.forEach(System.out::println);
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         
+        ArrayList<Integer> numbers = new ArrayList() {{
+            add(1);
+            add(2);
+            add(3);
+            add(4);
+            add(5);
+        }};
+        
+        printValues(numbers);
+        sumArraySquared(numbers);
     }
     
 }
