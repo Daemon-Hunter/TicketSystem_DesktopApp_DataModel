7c03394080ac31a7ab169839621f39e679252da3
DomGarbett <dominic.garbett@students.plymouth.ac.uk>
2016-03-21 17:57:19 +0000

    Social Media change (getsocialmedia)

--- Desktop_Datamodel/src/datamodel/ISocial.java
+++ Desktop_Datamodel/src/datamodel/ISocial.java
@@ -17,6 +17,9 @@ public interface ISocial extends IDbSubject {
     Integer getSocialId();
     Boolean setSocialId(Integer id);
     
+    SocialMedia getSocialMedia();
+    Boolean setSocialMedia(SocialMedia social);
+    
     URL getImage();
     Boolean setImage(URL img);
     
