6b44397a740d9fec9aceba28913ce7980d799e8d
Joshua Kellaway <dennis358.jk@gmail.com>
2016-04-06 16:51:43 +0100

    Wrapping things up

--- Desktop_Datamodel/src/wrappers/DesktopWrapper.java
+++ Desktop_Datamodel/src/wrappers/DesktopWrapper.java
@@ -5,125 +5,168 @@
  */
 package wrappers;
 
-import datamodel.Artist;
 import datamodel.IArtist;
 import datamodel.IParentEvent;
 import datamodel.IVenue;
-import datamodel.ParentEvent;
-import datamodel.Venue;
+import java.util.ArrayList;
 import java.util.List;
-import people.Admin;
 import people.IAdmin;
 import people.IUser;
-import people.User;
 /**
  *
  * @author 10512691
  */
 public class DesktopWrapper implements IDesktopWrapper {
-    List<ParentEvent> parentEventArray;
-    List<Venue>       venueArray;
-    List<Artist>      artistArray;
-    List<User>        userList;
-    List<Admin>       adminList;
+    List<IParentEvent> parentEventArray;
+    List<IVenue>       venueArray;
+    List<IArtist>      artistArray;
+    List<IUser>        userArray;
+    List<IAdmin>       adminArray;
 
+    public DesktopWrapper(){}
+    
     @Override
     public Boolean addUser(IUser user) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (userArray == null){
+            userArray = new ArrayList();
+        }
+        if (user == null){
+            throw new IllegalArgumentException("Cannot add null user.");
+        }
+        return userArray.add(user);
     }
 
     @Override
     public List<IUser> getUsers() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new ArrayList(userArray);
     }
 
     @Override
     public Boolean removeUser(IUser user) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Boolean addAdmin(IAdmin admin) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public IAdmin getAdmin(Integer adminID) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public List<IAdmin> getAdmins() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Boolean removeAdmin(IAdmin admin) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Boolean addParentEvent(IParentEvent pEvent) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public IParentEvent getParentEvent(Integer parentEventID) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public List<IParentEvent> getParentEvents() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Boolean removeParentEvent(IParentEvent pEvent) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Boolean addVenue(IVenue venue) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public IVenue getVenue(Integer venueID) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public List<IVenue> getVenues() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Boolean removeVenue(IVenue venue) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Boolean addArtist(IArtist artist) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public IArtist getArtist(Integer artistID) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public List<IArtist> getArtists() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Boolean removeArtist(IArtist artist) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (user == null){
+            throw new IllegalArgumentException("Cannot remove a null user");
+        }
+        return userArray.remove(user);
     }
 
     @Override
     public IUser getUser(Integer index) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return userArray.get(index);
+    }
+
+    @Override
+    public Boolean addAdmin(IAdmin admin) {
+        if (adminArray == null){
+            adminArray = new ArrayList();
+        }
+        if (admin == null){
+            throw new IllegalArgumentException("Cannot add a null admin");
+        }
+        return adminArray.add(admin);
+    }
+
+    @Override
+    public IAdmin getAdmin(Integer adminID) {
+        return adminArray.get(adminID);
+    }
+
+    @Override
+    public List<IAdmin> getAdmins() {
+        return new ArrayList(adminArray);
+    }
+
+    @Override
+    public Boolean removeAdmin(IAdmin admin) {
+        if(admin == null){
+            throw new IllegalArgumentException("Cannot remove a null admin.");
+        }
+        return adminArray.remove(admin);
+    }
+
+        @Override
+    public Boolean addParentEvent(IParentEvent pEvent) {
+        if (parentEventArray == null){
+            parentEventArray = new ArrayList();
+        }
+        if (pEvent == null){
+            throw new IllegalArgumentException("Cannot set user to null.");
+        }
+        return this.parentEventArray.add(pEvent);
+    }
+
+    @Override
+    public IParentEvent getParentEvent(Integer index) {
+        return parentEventArray.get(index);
+    }
+
+    @Override
+    public List<IParentEvent> getParentEvents() {
+        return new ArrayList(parentEventArray);
+    }
+
+    @Override
+    public Boolean removeParentEvent(IParentEvent pEvent) {
+        if (pEvent == null){
+            throw new IllegalArgumentException("Cannot remove null value.");
+        }
+        return parentEventArray.remove(pEvent);
+    }
+
+    @Override
+    public Boolean addVenue(IVenue venue) {
+        if (venueArray == null){
+            venueArray = new ArrayList();
+        }
+        if (venue == null){
+            throw new IllegalArgumentException("Cannot add null venue.");
+        }
+        return venueArray.add(venue);
+    }
+
+    @Override
+    public IVenue getVenue(Integer index) {
+        return venueArray.get(index);
+    }
+
+    @Override
+    public List<IVenue> getVenues() {
+        return new ArrayList(venueArray);
+    }
+
+    @Override
+    public Boolean removeVenue(IVenue venue) {
+        if(venue == null){
+            throw new IllegalArgumentException("Cannot remove a null venue.");
+        }
+        return venueArray.remove(venue);
+    }
+
+    @Override
+    public Boolean addArtist(IArtist artist) {
+        if (artistArray == null){
+            artistArray = new ArrayList();
+        }
+        if(artist == null){
+            throw new IllegalArgumentException("Cannot add a null artist.");
+        }
+        return artistArray.add(artist);
+    }
+
+    @Override
+    public IArtist getArtist(Integer artistID) {
+        return artistArray.get(artistID);
+    }
+
+    @Override
+    public List<IArtist> getArtists() {
+        return new ArrayList(artistArray);
+    }
+
+    @Override
+    public Boolean removeArtist(IArtist artist) {
+        if (artist == null){
+            throw new IllegalArgumentException("Cannot remove a null artist.");
+        }
+        return artistArray.remove(artist);
     }
 }
